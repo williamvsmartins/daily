dd3f9c5ec9a55ff504a0654d2a858c827110f9e2
WilliamVSMartins <williamvaltherprogramador@gmail.com>
2022-11-14 16:48:57 -0300

    feat(Server): config API

--- Server/config/config.js
+++ Server/config/config.js
@@ -1,5 +1,17 @@
 require('dotenv').config()
+require('../models/mongoose').connect().catch(err => console.log(err))
 
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+
+module.exports.setUpServer = () => {
+    const app = express()
+  
+    app.use(express.json())
+    app.use(cors())
+    app.use(morgan('tiny'))
+    app.use(require('../controllers'))
+  
+    return app
+  }
