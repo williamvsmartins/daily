c7d0407d01c860a58fa56adc78322df98a186c30
WilliamVSMartins <williamvaltherprogramador@gmail.com>
2022-12-26 20:15:03 -0300

    refactor(getAccount): query user from id

--- Server/controllers/account/getAccount.js
+++ Server/controllers/account/getAccount.js
@@ -4,7 +4,7 @@ module.exports.getOne = async (req, res) => {
     if(!req.params.account) res.status(400).send("Parâmetros vazios. Informe uma conta válida")
 
     try {
-        const account = await Account.findOne({account: req.params.account})
+        const account = await Account.findOne({ _id: req.params.account})
         return res.status(200).send(account)
     } catch (err) {
         return req.status(500).send(err)
